f9ee76bb16d1feb9e0e395b1b0e58a1265ad8112
uuutee <uuuteenet@gmail.com>
2017-09-23 00:29:50 +0900

    Remove ejs-render-loader & clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 // for ejs
 const fromPairs = require('lodash/frompairs');
@@ -29,7 +28,6 @@ module.exports = [
       filename: 'bundle.js'
     },
     plugins: [
-      new CleanWebpackPlugin(['dist']),
       new webpack.optimize.UglifyJsPlugin({
         sourceMap: true,
         compress: {
